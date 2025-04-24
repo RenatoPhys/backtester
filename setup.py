bc13054ed834c723ef04bd6140736b0482e3941d
RenatoPhys <renato.critelli.ifusp@gmail.com>
2025-04-24 10:22:10 -0300

    primeiro build

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-
 from setuptools import setup, find_packages
 
 setup(
-    name="backtester",
+    name="forex_backtester",  # Modificado de "backtester" para "forex_backtester"
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
         "pandas",
-        "matplotlib",
+        "matplotlib", 
         "pyarrow",
         "numba"
     ],
